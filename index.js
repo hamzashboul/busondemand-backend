664d10306f07a079a3d3d19a72f898cf8eef0f35
hamzashboul <hamzashboul282@gmail.com>
2026-05-08 15:46:13 +0300

    Fix CORS for new domain

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.use(helmet())
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'https://busondemand.vercel.app',
     'https://busondemand-frontend.vercel.app',
     process.env.FRONTEND_URL
   ].filter(Boolean),
